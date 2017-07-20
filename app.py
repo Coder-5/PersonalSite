cefab00d16f40a64290365fd0c2e4f5835843a3c
Salem Mufarreh <salemmuf2001@hotmail.com>
2017-07-20 11:37:55 +0300

    new ypdate on home page and spell corection

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ import random
 
 app = Flask(__name__)
 #flask name
-@app.route('/')
-def hello():
-	return "<h1>hello, world</h1>"
-@app.route('/goodbye')
-def goodbye():
-	return "<h1>goodbye, world</h1>"
+
 @app.route('/quote')
 def quote():
 	Quotes=["<h2>let your life be guided by gratnes</h2>","<h2>be the best version of your self</h2>", "<h2>all great leaders are Readers</h2>","<h2>be patient everything is coming together</h2>","<h2>silence is better than bulshit</h2> "]
 	return (random.choice(Quotes))
+@app.route('/')
 @app.route('/home')
 def homepage():
 	return render_template("home.html")
